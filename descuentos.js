6202c8338e8418a996ed4998f231e406f3774447
cristiangonzalo <cristiangonzalo@gmacservice.com>
2022-02-12 11:11:43 -0300

    Creación de función para calcular descuento de un precio

---
+++ descuentos.js
@@ -0,0 +1,20 @@
+console.group("Porcentajes y Descuentos");
+
+// const precioOriginal = 120;
+// const descuento = 18;
+
+
+/*console.log({
+    precioOriginal,
+    descuento,
+    porcentajePrecioConDescuento,
+    precioConDescuento
+});*/
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio  * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento;
+}
+
+console.groupEnd();
