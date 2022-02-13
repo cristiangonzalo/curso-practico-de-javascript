8d14704d0e35204dcda91988bf58ab192640398b
cristiangonzalo <cristiangonzalo@gmacservice.com>
2022-02-12 23:56:59 -0300

    Creación del formulario en HTML para calcular precio final del producto con descuento

--- descuentos.js
+++ descuentos.js
@@ -16,5 +16,11 @@ function calcularPrecioConDescuento(precio, descuento){
     const precioConDescuento = (precio  * porcentajePrecioConDescuento) / 100;
     return precioConDescuento;
 }
-
+function calcularPrecioFinal(){
+    const precioProducto = document.getElementById("precioProducto").value;
+    const descuentoProducto = document.getElementById("descuentoProducto").value;
+    const resultadoPrecioFinal = calcularPrecioConDescuento(Number(precioProducto), Number(descuentoProducto));
+    console.log(`El precio final del producto con descuento es: ${resultadoPrecioFinal}`);
+    document.getElementById("resultadoPrecioFinal").innerHTML = `El precio final del producto con descuento es: ${resultadoPrecioFinal}`;
+}
 console.groupEnd();
